c13500b9ff4ee0ee45e8836dfd90e6446f0d5e08
Jaein <mangowodls@gmail.com>
2025-05-17 11:21:36 +0900

    feat: swagger 설정

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import apiDocsBuilder from '../libs/shared/swagger/swagger-builder';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('GATEWAY_PORT') || 3000;
+
+  apiDocsBuilder(
+    app,
+    '[박재인] Event Reward System API Document',
+    '이벤트 보상 시스템 API 문서입니다.',
+  );
+
+  await app.listen(port);
 }
 bootstrap();
